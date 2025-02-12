a0d62223896a9dfd8d827f8a9786a38f9007b003
HorKanhchakna <kanhchakna1@gmail.com>
2025-02-12 14:35:59 +0700

    Add files via upload

---
+++ FormTest_app/FormTest/Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmSignUp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panelleft = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panelright = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.BtnSignUp = New System.Windows.Forms.Button()
+        Me.IconButton1 = New FontAwesome.Sharp.IconButton()
+        Me.TextPassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Textname = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.IconPictureBox1 = New FontAwesome.Sharp.IconPictureBox()
+        Me.IconPictureBox2 = New FontAwesome.Sharp.IconPictureBox()
+        Me.IconPictureBox3 = New FontAwesome.Sharp.IconPictureBox()
+        Me.Panelleft.SuspendLayout()
+        Me.Panelright.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.IconPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.IconPictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.IconPictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panelleft
+        '
+        Me.Panelleft.BackColor = System.Drawing.Color.LightGreen
+        Me.Panelleft.Controls.Add(Me.Button1)
+        Me.Panelleft.Controls.Add(Me.Label5)
+        Me.Panelleft.Controls.Add(Me.Label4)
+        Me.Panelleft.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panelleft.Location = New System.Drawing.Point(0, 0)
+        Me.Panelleft.Name = "Panelleft"
+        Me.Panelleft.Size = New System.Drawing.Size(475, 500)
+        Me.Panelleft.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Label1.Font = New System.Drawing.Font("Mongolian Baiti", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.ForestGreen
+        Me.Label1.Location = New System.Drawing.Point(193, 61)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 37)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Sign Up"
+        '
+        'Panelright
+        '
+        Me.Panelright.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Panelright.Controls.Add(Me.IconPictureBox3)
+        Me.Panelright.Controls.Add(Me.IconPictureBox2)
+        Me.Panelright.Controls.Add(Me.IconPictureBox1)
+        Me.Panelright.Controls.Add(Me.Panel3)
+        Me.Panelright.Controls.Add(Me.Panel1)
+        Me.Panelright.Controls.Add(Me.BtnSignUp)
+        Me.Panelright.Controls.Add(Me.IconButton1)
+        Me.Panelright.Controls.Add(Me.TextPassword)
+        Me.Panelright.Controls.Add(Me.Label3)
+        Me.Panelright.Controls.Add(Me.Textname)
+        Me.Panelright.Controls.Add(Me.Label2)
+        Me.Panelright.Controls.Add(Me.Label1)
+        Me.Panelright.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panelright.Location = New System.Drawing.Point(433, 0)
+        Me.Panelright.Name = "Panelright"
+        Me.Panelright.Size = New System.Drawing.Size(567, 500)
+        Me.Panelright.TabIndex = 1
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.DarkGreen
+        Me.Panel3.Location = New System.Drawing.Point(79, 351)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(453, 4)
+        Me.Panel3.TabIndex = 14
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.DarkGreen
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(79, 234)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(453, 4)
+        Me.Panel1.TabIndex = 13
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.DarkGreen
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(453, 4)
+        Me.Panel2.TabIndex = 14
+        '
+        'BtnSignUp
+        '
+        Me.BtnSignUp.BackColor = System.Drawing.Color.LightGreen
+        Me.BtnSignUp.Font = New System.Drawing.Font("Mongolian Baiti", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSignUp.Location = New System.Drawing.Point(178, 399)
+        Me.BtnSignUp.Name = "BtnSignUp"
+        Me.BtnSignUp.Size = New System.Drawing.Size(200, 60)
+        Me.BtnSignUp.TabIndex = 12
+        Me.BtnSignUp.Text = "Sign Up"
+        Me.BtnSignUp.UseVisualStyleBackColor = False
+        '
+        'IconButton1
+        '
+        Me.IconButton1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.IconButton1.IconChar = FontAwesome.Sharp.IconChar.Remove
+        Me.IconButton1.IconColor = System.Drawing.Color.DarkGreen
+        Me.IconButton1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton1.Location = New System.Drawing.Point(517, 0)
+        Me.IconButton1.Name = "IconButton1"
+        Me.IconButton1.Size = New System.Drawing.Size(50, 50)
+        Me.IconButton1.TabIndex = 11
+        Me.IconButton1.UseVisualStyleBackColor = False
+        '
+        'TextPassword
+        '
+        Me.TextPassword.Font = New System.Drawing.Font("Mongolian Baiti", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextPassword.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.TextPassword.Location = New System.Drawing.Point(79, 323)
+        Me.TextPassword.Name = "TextPassword"
+        Me.TextPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextPassword.Size = New System.Drawing.Size(453, 32)
+        Me.TextPassword.TabIndex = 5
+        Me.TextPassword.Text = "Password"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Label3.Font = New System.Drawing.Font("Mongolian Baiti", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.ForestGreen
+        Me.Label3.Location = New System.Drawing.Point(70, 278)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(110, 25)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Password:"
+        '
+        'Textname
+        '
+        Me.Textname.Font = New System.Drawing.Font("Mongolian Baiti", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Textname.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Textname.Location = New System.Drawing.Point(79, 206)
+        Me.Textname.Name = "Textname"
+        Me.Textname.Size = New System.Drawing.Size(453, 32)
+        Me.Textname.TabIndex = 3
+        Me.Textname.Text = "Full Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Label2.Font = New System.Drawing.Font("Mongolian Baiti", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.ForestGreen
+        Me.Label2.Location = New System.Drawing.Point(70, 161)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(127, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "User Name:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Label4.Font = New System.Drawing.Font("Mongolian Baiti", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Black
+        Me.Label4.Location = New System.Drawing.Point(143, 71)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(134, 37)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Sign Up"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Label5.Font = New System.Drawing.Font("Mongolian Baiti", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Black
+        Me.Label5.Location = New System.Drawing.Point(73, 163)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(296, 23)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "SignUp Now To connect with us!"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LightGreen
+        Me.Button1.Font = New System.Drawing.Font("Mongolian Baiti", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(102, 259)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 60)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "Sign Up"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'IconPictureBox1
+        '
+        Me.IconPictureBox1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.IconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.IconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Google
+        Me.IconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText
+        Me.IconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconPictureBox1.Location = New System.Drawing.Point(188, 111)
+        Me.IconPictureBox1.Name = "IconPictureBox1"
+        Me.IconPictureBox1.Size = New System.Drawing.Size(32, 32)
+        Me.IconPictureBox1.TabIndex = 15
+        Me.IconPictureBox1.TabStop = False
+        '
+        'IconPictureBox2
+        '
+        Me.IconPictureBox2.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.IconPictureBox2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.IconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.Facebook
+        Me.IconPictureBox2.IconColor = System.Drawing.SystemColors.ControlText
+        Me.IconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconPictureBox2.Location = New System.Drawing.Point(248, 111)
+        Me.IconPictureBox2.Name = "IconPictureBox2"
+        Me.IconPictureBox2.Size = New System.Drawing.Size(32, 32)
+        Me.IconPictureBox2.TabIndex = 16
+        Me.IconPictureBox2.TabStop = False
+        '
+        'IconPictureBox3
+        '
+        Me.IconPictureBox3.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.IconPictureBox3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.IconPictureBox3.IconChar = FontAwesome.Sharp.IconChar.Linkedin
+        Me.IconPictureBox3.IconColor = System.Drawing.SystemColors.ControlText
+        Me.IconPictureBox3.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconPictureBox3.Location = New System.Drawing.Point(305, 111)
+        Me.IconPictureBox3.Name = "IconPictureBox3"
+        Me.IconPictureBox3.Size = New System.Drawing.Size(32, 32)
+        Me.IconPictureBox3.TabIndex = 17
+        Me.IconPictureBox3.TabStop = False
+        '
+        'FrmSignUp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightCoral
+        Me.ClientSize = New System.Drawing.Size(1000, 500)
+        Me.Controls.Add(Me.Panelright)
+        Me.Controls.Add(Me.Panelleft)
+        Me.Font = New System.Drawing.Font("Mongolian Baiti", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "FrmSignUp"
+        Me.Text = "FormSignUp"
+        Me.Panelleft.ResumeLayout(False)
+        Me.Panelleft.PerformLayout()
+        Me.Panelright.ResumeLayout(False)
+        Me.Panelright.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.IconPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.IconPictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.IconPictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panelleft As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panelright As Panel
+    Friend WithEvents TextPassword As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Textname As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents BtnSignUp As Button
+    Friend WithEvents IconButton1 As FontAwesome.Sharp.IconButton
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents IconPictureBox2 As FontAwesome.Sharp.IconPictureBox
+    Friend WithEvents IconPictureBox1 As FontAwesome.Sharp.IconPictureBox
+    Friend WithEvents IconPictureBox3 As FontAwesome.Sharp.IconPictureBox
+End Class
